bee46041cf722fc39a08ab9ecfe59ecda81f8424
CLMNTDFR <deferclement59@gmail.com>
2024-12-09 14:06:42 +0100

    Added conversion of Markdown ordered lists to HTML in conversion script

--- markdown2html.py
+++ markdown2html.py
@@ -65,37 +65,20 @@ def convert_markdown_ul_list_to_html(lines):
 
 
 def convert_markdown_ol_list_to_html(lines):
-    """
-    Convert Markdown ordered list syntax to HTML.
-
-    Args:
-        lines (list): List of lines from the Markdown file.
-
-    Returns:
-        list: List of converted lines with HTML ordered list.
-    """
     in_list = False
     html_lines = []
 
     for line in lines:
-        if line.lstrip().startswith(
-            tuple(f"{i}. " for i in range(1, 10))
-        ):  # If line starts with '1. ', '2. ', etc.
-            line_content = line.split(". ", 1)[
-                1
-            ].strip()  # Extract text after the number and dot
+        if line.startswith("* "):
+            line_content = line[2:].strip()
             if not in_list:
-                # Open an <ol> tag for the list
                 html_lines.append("<ol>\n")
                 in_list = True
-            # Convert each item to <li>
             html_lines.append(f"   <li>{line_content}</li>\n")
         else:
             if in_list:
-                # Close the <ol> tag at the end of the list
                 html_lines.append("</ol>\n")
                 in_list = False
-            # Add the line without modification
             html_lines.append(line)
 
     if in_list:
@@ -111,8 +94,7 @@ def main():
     """
     # Check if the number of arguments is sufficient
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html",
-              file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
 
     # Get the file names from the arguments
